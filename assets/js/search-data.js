4b971ef77014016ea2a3e6a6811121e61537f57a
RominaH <70785656+RominaH@users.noreply.github.com>
2026-05-03 02:10:05 +0000

    Deploying to gh-pages from @ RominaH/RominaH.github.io@5a1b05e7902d89c661c62698bf7b010d10c248a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,8 +416,8 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-mutlimodal-social-media-constructions",
+          title: 'mutlimodal social media constructions',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
