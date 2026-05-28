4156283d38d362de7656e0a851b6f6172445e6c0
RominaH <70785656+RominaH@users.noreply.github.com>
2026-05-28 05:10:28 +0000

    Deploying to gh-pages from @ RominaH/RominaH.github.io@de5e4c53037f9bf15bb3880c6c7bebdd9fa09ff2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,11 +416,6 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-so-yeah-particles",
-          title: 'So Yeah particles',
-          description: "Topic-management with “so yeah” in English interaction",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{id: "projects-ab-before-ba",
           title: 'AB Before BA',
           description: "",
