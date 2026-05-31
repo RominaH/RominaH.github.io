9263aec573b36d543a1ac13a1e1cacd2dcc81b19
RominaH <70785656+RominaH@users.noreply.github.com>
2026-05-31 20:50:58 +0000

    Deploying to gh-pages from @ RominaH/RominaH.github.io@584a2aa95af841be8dbd125b1e5c4838476ef926 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -393,12 +393,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-ll-be-giving-a-talk-on-slogans-and-schemes-at-the-rhetcanada-conference-at-sju-in-may",
+      },{id: "news-i-ll-be-giving-a-talk-on-slogans-and-schemes-at-the-rhetcanada-conference-at-sju-in-may",
           title: 'I’ll be giving a talk on slogans and schemes at the RhetCanada conference...',
           description: "",
           section: "News",},{id: "news-rhetoricon-symposium",
